288198217e326499b19c8c07ff8e1c2c4674e15e
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-10-28 13:03:52 -0500

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import type { Component } from 'vue';
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App as Component).mount('#app');
